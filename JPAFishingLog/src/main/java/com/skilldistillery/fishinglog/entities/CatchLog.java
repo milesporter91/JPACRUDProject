f96f6c882e78b806c48195d9ea54ad383f4eb338
Miles Porter <portermiles91@gmail.com>
2025-05-02 17:17:45 -0600

    create entity class and JUnit Test

---
+++ JPAFishingLog/src/main/java/com/skilldistillery/fishinglog/entities/CatchLog.java
@@ -0,0 +1,171 @@
+package com.skilldistillery.fishinglog.entities;
+
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="catch_log")
+public class CatchLog {
+	// Fields
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	private int id;
+	private String species;
+	
+	@Column(name="scientific_name")
+	private String scientificName;
+	
+	@Column(name="weight_lbs")
+	private String weightInLbs;
+	
+	@Column(name="length_inches")
+	private String lengthInInches;
+	
+	@Column(name="bait_used")
+	private String baitUsed;
+	
+	@Column(name="catch_and_release")
+	private boolean catchAndRelease;
+	
+	@Column(name="date_caught")
+	private LocalDate dateCaught;
+	
+	@Column(name="time_caught")
+	private LocalTime timeCaught;
+	
+	private Double latitude;
+	private Double longitude;
+	private String notes;
+	
+	@Column(name="image_url")
+	private String imageUrl;
+
+	// Constructors
+	public CatchLog() {
+		super();
+	}
+	
+	// Getters & Setters
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getSpecies() {
+		return species;
+	}
+
+	public void setSpecies(String species) {
+		this.species = species;
+	}
+
+	public String getScientificName() {
+		return scientificName;
+	}
+
+	public void setScientificName(String scientificName) {
+		this.scientificName = scientificName;
+	}
+
+	public String getWeightInLbs() {
+		return weightInLbs;
+	}
+
+	public void setWeightInLbs(String weightInLbs) {
+		this.weightInLbs = weightInLbs;
+	}
+
+	public String getLengthInInches() {
+		return lengthInInches;
+	}
+
+	public void setLengthInInches(String lengthInInches) {
+		this.lengthInInches = lengthInInches;
+	}
+
+	public String getBaitUsed() {
+		return baitUsed;
+	}
+
+	public void setBaitUsed(String baitUsed) {
+		this.baitUsed = baitUsed;
+	}
+
+	public boolean isCatchAndRelease() {
+		return catchAndRelease;
+	}
+
+	public void setCatchAndRelease(boolean catchAndRelease) {
+		this.catchAndRelease = catchAndRelease;
+	}
+
+	public LocalDate getDateCaught() {
+		return dateCaught;
+	}
+
+	public void setDateCaught(LocalDate dateCaught) {
+		this.dateCaught = dateCaught;
+	}
+
+	public LocalTime getTimeCaught() {
+		return timeCaught;
+	}
+
+	public void setTimeCaught(LocalTime timeCaught) {
+		this.timeCaught = timeCaught;
+	}
+
+	public Double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(Double latitude) {
+		this.latitude = latitude;
+	}
+
+	public Double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(Double longitude) {
+		this.longitude = longitude;
+	}
+
+	public String getNotes() {
+		return notes;
+	}
+
+	public void setNotes(String notes) {
+		this.notes = notes;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	// toString
+	@Override
+	public String toString() {
+		return "Catch Log ID: " + id + "\nSpecies: " + species + "\nScientific Name: " + scientificName + "\nWeight in Pounds: "
+				+ weightInLbs + "\nLength in Inches" + lengthInInches + "\nBait Used: " + baitUsed + "\nCatch and Release: "
+				+ catchAndRelease + "\nDate Caught: " + dateCaught + "\nTime Caught: " + timeCaught
+				+ "\nLocation: \n\tLatitude: " + latitude + " Longitude: " + longitude + "\nNotes: " + notes + "\nImage URL: " + imageUrl;
+	}
+	
+
+}
---
+++ JPAFishingLog/src/test/java/com/skilldistillery/fishinglog/entities/CatchLogTest.java
@@ -0,0 +1,50 @@
+package com.skilldistillery.fishinglog.entities;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+class CatchLogTest {
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	CatchLog catchLog;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("JPAFishingLog");
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+		emf.close();
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		catchLog = em.find(CatchLog.class, 1);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		em.close();
+		catchLog = null;
+	}
+
+	@Test
+	void test() {
+		assertNotNull(catchLog);
+		assertEquals("Largemouth Bass".toLowerCase(), catchLog.getSpecies().toLowerCase());
+		assertEquals("Spinner".toLowerCase(), catchLog.getBaitUsed().toLowerCase());
+	}
+
+}
