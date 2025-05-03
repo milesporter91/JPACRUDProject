677c4b7675bf25cebf7655b0d7db81aead84c90c
Miles Porter <portermiles91@gmail.com>
2025-05-02 18:17:52 -0600

    Create Spring Boot MVC Project, configure dependencies

---
+++ FishingLog/src/main/java/com/skilldistillery/fishinglog/FishingLogApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.fishinglog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishingLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishingLogApplication.class, args);
+	}
+
+}
---
+++ FishingLog/src/test/java/com/skilldistillery/fishinglog/FishingLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.fishinglog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishingLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
